ffaa0a8c40b91d113234654467c862dedece684b
Shafin The Developer <sahariar350@gmail.com>
2021-04-07 20:05:58 +0600

    Some api added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const { response } = require('express');
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -13,8 +15,42 @@ app.use(bodyParser.json());
 const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.qvg4g.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    const collection = client.db(`${process.env.DB_NAME}`).collection("dishes");
-    console.log(err);
+    const collection = client.db(`${ process.env.DB_NAME }`).collection("dishes");
+
+    app.post('/addProduct', (req, res) => {
+        collection.insertOne(req.body)
+            .then(req => {
+                res.send("Success!!")
+            })
+    })
+
+    app.get('/products', (req, res) => {
+        const dishCategory = req.query.category;
+        console.log(dishCategory);
+        collection.find({
+            category: dishCategory
+        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/productsForManage', (req, res) => {
+        collection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.delete('/delete', (req, res) => {
+        console.log(req.query.id);
+        collection.deleteOne({
+            _id: ObjectID(req.query.id)
+        })
+        .then(response => {
+            console.log(response);
+        })
+    })
 
 });
 
