a1f7c83ac76c02dbfc15ec42d1211ad5726a1e78
Shafin The Developer <sahariar350@gmail.com>
2021-04-09 22:27:34 +0600

    something is cooking

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db(`${ process.env.DB_NAME }`).collection("dishes");
-    const order = client.db(`${ process.env.DB_NAME }`).collection("orders");
+    // const order = client.db(`${ process.env.DB_NAME }`).collection("orders");
 
     app.post('/addProduct', (req, res) => {
         collection.insertOne(req.body)
